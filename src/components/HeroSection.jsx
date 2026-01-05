ed30bb3ffb9040169d029098c083f7431fb12f37
nahom70 <terrefen@berea.edu>
2026-01-05 01:07:00 -0500

    Added Navbar,fonts,colors; Not responsive yet

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const HeroSection = () => {
   return (
-    <div>
-        
+    <div className='bg-primary-950 h-screen pt-20'>
+        <Navbar/>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { HiArrowLongRight } from "react-icons/hi2";
 
 const Navbar = () => {
   return (
-    <div></div>
+    <>
+    <div className='flex items-baseline px-20 rounded-2xl w-full justify-between text-text-600 font-raptor-bold'>
+      <div>
+        <h1 className='text-9xl'>UnBlock</h1>
+      </div>
+      <div>
+        <button>Sign in</button>
+      </div>
+    </div>
+
+    <div className='text-text-200 text-4xl absolute top-[40%] right-0 p-10 font-raptor-medium flex flex-col gap-5 *:cursor-pointer'>
+      <span className='flex'><HiArrowLongRight/> How  it works</span>
+      <span className='flex'><HiArrowLongRight/> Why Unblock</span>
+      <span className='flex'><HiArrowLongRight/> About</span>
+    </div>
+    </>
   )
 }
 
